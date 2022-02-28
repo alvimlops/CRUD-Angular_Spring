3efb595edaeed28256aa2118e797095bbff528fa
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-02-27 21:20:10 -0300

    Lista de cursos: spinner (carregando)

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,7 +10,7 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable <Course[]>;
   displayedColumns = ['name', 'categorya'];
   //CoursesService: CoursesService;
 
@@ -17,6 +18,8 @@ export class CoursesComponent implements OnInit {
    // this.courses = [];
    //this.CoursesService = new CoursesService();
    this.courses = this.CoursesService.list();
+
+   //this.CoursesService.list().subscribe(courses => this.courses = courses)
    }
 
   ngOnInit(): void {
